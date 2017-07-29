45502ba6b65d11930c8c81b3081a082b5897f356
zmixon <zmixon@vynyl.com>
2017-07-29 15:57:04 -0600

    Initialize Node

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+// Define Port
+app.set('port', 3000)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Listen for requests
+const server = app.listen(app.get('port'), () => {
+  const port = server.address().port
+  console.log(`Server listening on port: ${port}`)
+})
